ed339e58aeb74eb4bbd72f2489e4a8bbada0c1aa
Santi2007939 <117610911+Santi2007939@users.noreply.github.com>
2026-05-12 17:37:58 -0500

    build: add i18n, pinia and router for main.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import pinia from "./pinia.js";
+import i18n from "./i18n.js";
+import router from "./router.js";
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(pinia)
+    .use(i18n)
+    .use(router)
+    .mount('#app')
